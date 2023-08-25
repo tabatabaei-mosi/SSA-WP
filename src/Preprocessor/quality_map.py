49368fd450522113beffb14b33971011f9af6b8e
tabatabaei-mostafa <tabatabaei.mosi@gmail.com>
2023-08-25 12:36:35 +0330

    Debug path conflict in preprocessor

--- src/Preprocessor/quality_map.py
+++ src/Preprocessor/quality_map.py
@@ -9,11 +9,11 @@ from utils import path_check
 
 
 def get_prop(
-    file: str, 
-    keyword_list: list, 
-    size: tuple = (85, 185, 31), 
+    file: str,
+    keyword_list: list,
+    size: tuple = (85, 185, 31),
     mode: str = 'matrix'
-    ):
+):
     """
     Read properties from a .GRID file and return them based on the specified mode.
 
@@ -30,17 +30,17 @@ def get_prop(
     """
     result_matrix = []
     result_list = []
-    
+
     # Read file until it reachs to keyword and Do until / finds in line.
     for key in keyword_list:
         with open(file, 'r') as grid_file:
-            
+
             # target is a list of lines after the keyword.
             target = []
-            
+
             # i is a counter to skip the first line after the keyword.
             i = 0
-            
+
             # Read file until it reachs to keyword.
             for line in grid_file:
                 # If the line starts with the keyword, then read the next line.
@@ -57,7 +57,7 @@ def get_prop(
                         else:
                             key_line = next(grid_file)
                             target.append(key_line)
-                            
+
         # that key should add to prop_list in type of list.
         prop_list = []
         for line in target:
@@ -68,7 +68,7 @@ def get_prop(
                 if '/' not in num:
                     if '*' in num:
                         split_num = num.split("*")
-                
+
                         if len(split_num) > 1:
                             times = split_num[0]
                             value = split_num[1]
@@ -79,8 +79,8 @@ def get_prop(
                         else:
                             # add the value to the list as float.
                             prop_list.append(float(split_num[0]))
-                            
-                    # add the value to the list as float. 
+
+                    # add the value to the list as float.
                     else:
                         prop_list.append(float(num))
 
@@ -101,13 +101,13 @@ def get_prop(
         return result_list
     else:
         return result_matrix, result_list
-    
-    
+
+
 def possible_location(
-    loc: tuple, 
-    null_space_lim: int = 2, 
+    loc: tuple,
+    null_space_lim: int = 2,
     grid_size: tuple = (85, 185, 31)
-    ):
+):
     """
     Determine possible locations based on given location and null space limit.
 
@@ -133,9 +133,9 @@ def possible_location(
 
 
 def find_null_blocks(
-    prop_dic: dict, 
+    prop_dic: dict,
     grid_size: tuple = (85, 185, 31)
-    ):
+):
     """
     Find null blocks in the property dictionary.
 
@@ -150,29 +150,29 @@ def find_null_blocks(
     """
     null_loc = []
     null_loc_0 = []
-    
+
     for k in range(grid_size[2]):
         for j in range(grid_size[1]):
             for i in range(grid_size[0]):
-                
+
                 # Check if all properties are 0. If so, add the location to the list (NULL BLUCK)
                 if all(prop_dic[f'{key}'][i, j, k] == 0 for key in prop_dic.keys()):
-                    
+
                     # (index start with 1) this will use later to check optimized location whether they are near to null block.
                     null_loc.append((i + 1, j + 1, k + 1))
-                    
+
                     # this is null block which use for cleaning map. index will start by 0.
                     null_loc_0.append((i, j, k))
-                    
+
     return null_loc, null_loc_0
 
 
 def rock_index(
-    file: str, 
-    prop_key_list: list, 
-    grid_size=(85, 185, 31), 
+    file: str,
+    prop_key_list: list,
+    grid_size=(85, 185, 31),
     threshold=1000
-    ):
+):
     """
     Calculate rock quality index and identify best RQI values and null blocks.
 
@@ -196,7 +196,8 @@ def rock_index(
     rock_quality_index = np.prod(prop_values, axis=0) * (1 - prop_dic['SWAT'])
 
     # [3] Slice up RQI matrix to Best RQI (list), Best RQI loc (list)
-    indices = np.where((rock_quality_index >= threshold) & (rock_quality_index <= rock_quality_index.max()))
+    indices = np.where((rock_quality_index >= threshold) & (
+        rock_quality_index <= rock_quality_index.max()))
     best_rqi_ = rock_quality_index[indices]
     best_rqi_id_ = list(zip(*indices))
 
@@ -207,9 +208,9 @@ def rock_index(
 
 
 def clean_borders(
-    best_rqi: list, best_rqi_loc: list, 
+    best_rqi: list, best_rqi_loc: list,
     grid_size: tuple = (85, 185, 31)
-    ):
+):
     """
     Clean best RQIs from border locations in x and y directions.
 
@@ -236,11 +237,11 @@ def clean_borders(
 
 
 def clean_nulls(
-    null_locs: list, 
-    best_rqi: list, rqi_locs: list, 
-    min_space: int = 2, 
+    null_locs: list,
+    best_rqi: list, rqi_locs: list,
+    min_space: int = 2,
     grid_dim: tuple = (85, 185, 31)
-    ):
+):
     """
     Clean RQI locations by ensuring a minimum distance from null blocks.
 
@@ -299,10 +300,10 @@ def clean_interval(
     rqi_list: List[float],
     rqi_matrix: np.ndarray,
     threshold: float = 1000
-    ) -> Tuple[List[Tuple[int, int, int]],
-        List[float],
-        List[Tuple[int, int, int, int]],
-        List[float]]:
+) -> Tuple[List[Tuple[int, int, int]],
+           List[float],
+           List[Tuple[int, int, int, int]],
+           List[float]]:
     """
     Clean and filter RQI values based on intervals.
 
@@ -318,7 +319,7 @@ def clean_interval(
               List[Tuple[int, int, int, int]],
               List[float]]: Filtered RQI locations, RQI values, interval locations, and interval RQI values.
     """
-    
+
     final_best_loc = []
     final_best_rqi = []
     ok_interval_loc = []
@@ -327,7 +328,8 @@ def clean_interval(
     for idx, loc in enumerate(rqi_loc_list):
         x, y, z_c = loc
         z_s, z_e = z_interval(z_c, grid_size=rqi_matrix.shape)
-        rqi_s, rqi_c, rqi_e = rqi_matrix[x, y, z_s], rqi_matrix[x, y, z_c], rqi_matrix[x, y, z_e]
+        rqi_s, rqi_c, rqi_e = rqi_matrix[x, y,
+                                         z_s], rqi_matrix[x, y, z_c], rqi_matrix[x, y, z_e]
 
         if all(rqi >= threshold for rqi in [rqi_s, rqi_c, rqi_e]):
             interval_loc = (x, y, z_s, z_e)
@@ -349,15 +351,16 @@ def z_interval(center_block: int,
 
 
 def filter_locs(
-    file: str, 
-    prop_key_list: List[str], 
-    null_space: int = 2, 
+    file: str,
+    prop_key_list: List[str],
+    null_space: int = 2,
     threshold: int = 1000,
-    grid_size: Tuple[int, int, int] = (85, 185, 31), 
+    grid_size: Tuple[int, int, int] = (85, 185, 31),
     check_interval: bool = True
-    ) -> Union[
-    Tuple[List[float], List[Tuple[int, int, int]], List[Tuple[int, int, int]], List[float], List[Tuple[int, int, int]]],
-    Tuple[List[float], List[Tuple[int, int, int]], List[Tuple[int, int, int]]]]:
+) -> Union[
+    Tuple[List[float], List[Tuple[int, int, int]],
+          List[Tuple[int, int, int]], List[float], List[Tuple[int, int, int]]],
+        Tuple[List[float], List[Tuple[int, int, int]], List[Tuple[int, int, int]]]]:
     """
     This function performs various filtering and cleaning operations on RQI (Rock Quality Index) data.
     It reads properties from a <.GRID> file, calculates RQI, filters null blocks and border locations,
@@ -394,19 +397,19 @@ def filter_locs(
 
     if check_interval:
         # Clean intervals and get final RQI values/locations after interval checking
-        final_rqi_locs_, final_rqi_, _, _ = clean_interval(final_rqi_locs, final_rqi, rqi_matrix, TH=threshold)
+        final_rqi_locs_, final_rqi_, _, _ = clean_interval(
+            final_rqi_locs, final_rqi, rqi_matrix, TH=threshold)
         return final_rqi, final_rqi_locs, null_locs, final_rqi_, final_rqi_locs_
     else:
         return final_rqi, final_rqi_locs, null_locs
-
 
 
 def main(
     rqi_limit=0,
     grid_path='INCLUDE/SSA_RQI_0.GRDECL',
     null_space=2,
-    prop_key = ['PERMX', 'PORO', 'NTG', 'TRANY', 'TRANZ', 'SWAT'],
-    dim = (85, 185, 31)
+    prop_key=['PERMX', 'PORO', 'NTG', 'TRANY', 'TRANZ', 'SWAT'],
+    dim=(85, 185, 31)
 ):
     """
     Main function to process RQI data.
@@ -418,29 +421,31 @@ def main(
         prop_key (List[str], optional): List of properties keywords. Defaults to ['PERMX', 'PORO', 'NTG', 'TRANY', 'TRANZ', 'SWAT'].
         dim (Tuple[int, int, int], optional): Dimensions of the grid. Defaults to (85, 185, 31).
     """
-    
+
     # Record the start time of the program.
     start_time = time()
 
     # Inputs
     rqi_limit = 0
-    
+
     abs_path = Path(__file__).resolve().parent.parent
     grid_path = f'{abs_path}/{grid_path}'
-    
-    rqi_path = f'{abs_path}/RQI/TH={rqi_limit}'  # RQI files will be saved here.
-    
+
+    # RQI files will be saved here.
+    rqi_path = f'{abs_path}/RQI/TH={rqi_limit}'
+
     # RQI map considering interval check will save here
     interval_path = f'{rqi_path}/interval_checked'
     path_check(interval_path)  # if path doesn't exist, create it.
 
     # Filter locations
-    best_rqi, best_rqi_locs, null_blocks, rqi_interval, locs_interval = filter_locs(grid_path, prop_key, null_space, rqi_limit, dim, check_interval=True)
+    best_rqi, best_rqi_locs, null_blocks, rqi_interval, locs_interval = filter_locs(
+        grid_path, prop_key, null_space, rqi_limit, dim, check_interval=True)
 
     # Write answers using loguru logger
     with open(f'{rqi_path}/best_RQI.text', 'a+') as rqi_file:
         rqi_file.write('\n'.join(str(rqi) for rqi in best_rqi))
-    
+
     with open(f'{rqi_path}/best_RQI_locs.text', 'a+') as rqi_locs_file:
         for item in best_rqi_locs:
             x, y, z = item[0], item[1], item[2]
@@ -468,13 +473,13 @@ def main(
 
 
 if __name__ == "__main__":
-    
-    rqi_limit=0
-    grid_path='INCLUDE/SSA_RQI_0.GRDECL'
-    null_space=2
+
+    rqi_limit = 0
+    grid_path = 'INCLUDE/SSA_RQI_0.GRDECL'
+    null_space = 2
     prop_key = ['PERMX', 'PORO', 'NTG', 'TRANY', 'TRANZ', 'SWAT']
     dim = (85, 185, 31)
-    
+
     main(
         rqi_limit=rqi_limit,
         grid_path=grid_path,
@@ -482,4 +487,3 @@ if __name__ == "__main__":
         prop_key=prop_key,
         dim=dim
     )
-
